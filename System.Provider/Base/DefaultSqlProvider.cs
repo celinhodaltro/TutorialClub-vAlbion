9b5466f539fdb45df666c1f11a37178aad0a2204
João Marcelo Daltro Marques <joao.marques@produsti.com.br>
2024-06-03 11:37:34 -0300

    Preparação de Provider

--- System.Provider/Base/DefaultSqlProvider.cs
+++ System.Provider/Base/DefaultSqlProvider.cs
@@ -12,65 +12,9 @@ namespace App.Provider
     {
         private readonly string _connectionString = "Server=your_server_address;Database=your_database_name;User Id=your_username;Password=your_password;";
 
-        public int Insert<T>(T item)
-        {
-            var type = typeof(T);
-            var tableName = type.Name;
-            var properties = type.GetProperties();
-            var columns = string.Join(", ", properties.Select(p => p.Name));
-            var values = string.Join(", ", properties.Select(p => $"@{p.Name}"));
 
-            var query = $"INSERT INTO {tableName} ({columns}) VALUES ({values})";
-
-            var parameters = properties.Select(p => new SqlParameter($"@{p.Name}", p.GetValue(item) ?? DBNull.Value)).ToList();
-            return ExecuteNonQuery(query, parameters);
-        }
-
-        public int Update<T>(T item, string keyProperty)
-        {
-            var type = typeof(T);
-            var tableName = type.Name;
-            var properties = type.GetProperties();
-            var key = properties.FirstOrDefault(p => p.Name == keyProperty);
-            if (key == null) throw new ArgumentException("Key property not found");
-
-            var setClause = string.Join(", ", properties.Where(p => p.Name != keyProperty).Select(p => $"{p.Name} = @{p.Name}"));
-            var query = $"UPDATE {tableName} SET {setClause} WHERE {keyProperty} = @{keyProperty}";
-
-            var parameters = properties.Select(p => new SqlParameter($"@{p.Name}", p.GetValue(item) ?? DBNull.Value)).ToList();
-            return ExecuteNonQuery(query, parameters);
-        }
-
-        public int Delete<T>(object key, string keyProperty)
-        {
-            var type = typeof(T);
-            var tableName = type.Name;
-            var query = $"DELETE FROM {tableName} WHERE {keyProperty} = @{keyProperty}";
-
-            var parameter = new SqlParameter($"@{keyProperty}", key);
-            return ExecuteNonQuery(query, new List<SqlParameter> { parameter });
-        }
-
-        public T Get<T>(object key, string keyProperty) where T : new()
-        {
-            var type = typeof(T);
-            var tableName = type.Name;
-            var query = $"SELECT * FROM {tableName} WHERE {keyProperty} = @{keyProperty}";
-
-            var parameter = new SqlParameter($"@{keyProperty}", key);
-            var result = ExecuteQuery<T>(query, new List<SqlParameter> { parameter });
-            return result.FirstOrDefault();
-        }
-
-        public List<T> GetAll<T>() where T : new()
-        {
-            var type = typeof(T);
-            var tableName = type.Name;
-            var query = $"SELECT * FROM {tableName}";
-            return ExecuteQuery<T>(query);
-        }
-
-        private List<T> ExecuteQuery<T>(string query, List<SqlParameter> parameters = null) where T : new()
+        #region ProviderBase
+        private List<T> ExecuteQuery<T>(string query, List<SqlParameter>? parameters = null) where T : new()
         {
             List<T> resultList = new List<T>();
 
@@ -138,5 +82,68 @@ namespace App.Provider
                 throw new Exception("Erro ao executar o comando SQL: " + ex.Message);
             }
         }
+
+        #endregion
+
+        #region Default
+        public int Insert<T>(T item)
+        {
+            var type = typeof(T);
+            var tableName = type.Name;
+            var properties = type.GetProperties();
+            var columns = string.Join(", ", properties.Select(p => p.Name));
+            var values = string.Join(", ", properties.Select(p => $"@{p.Name}"));
+
+            var query = $"INSERT INTO {tableName} ({columns}) VALUES ({values})";
+
+            var parameters = properties.Select(p => new SqlParameter($"@{p.Name}", p.GetValue(item) ?? DBNull.Value)).ToList();
+            return ExecuteNonQuery(query, parameters);
+        }
+
+        public int Update<T>(T item, string keyProperty)
+        {
+            var type = typeof(T);
+            var tableName = type.Name;
+            var properties = type.GetProperties();
+            var key = properties.FirstOrDefault(p => p.Name == keyProperty);
+            if (key == null) throw new ArgumentException("Key property not found");
+
+            var setClause = string.Join(", ", properties.Where(p => p.Name != keyProperty).Select(p => $"{p.Name} = @{p.Name}"));
+            var query = $"UPDATE {tableName} SET {setClause} WHERE {keyProperty} = @{keyProperty}";
+
+            var parameters = properties.Select(p => new SqlParameter($"@{p.Name}", p.GetValue(item) ?? DBNull.Value)).ToList();
+            return ExecuteNonQuery(query, parameters);
+        }
+
+        public int Delete<T>(object key, string keyProperty)
+        {
+            var type = typeof(T);
+            var tableName = type.Name;
+            var query = $"DELETE FROM {tableName} WHERE {keyProperty} = @{keyProperty}";
+
+            var parameter = new SqlParameter($"@{keyProperty}", key);
+            return ExecuteNonQuery(query, new List<SqlParameter> { parameter });
+        }
+
+        public T? Get<T>(object key, string keyProperty) where T : new()
+        {
+            var type = typeof(T);
+            var tableName = type.Name;
+            var query = $"SELECT * FROM {tableName} WHERE {keyProperty} = @{keyProperty}";
+
+            var parameter = new SqlParameter($"@{keyProperty}", key);
+            var result = ExecuteQuery<T>(query, new List<SqlParameter> { parameter });
+            return result.FirstOrDefault();
+        }
+
+        public List<T> GetAll<T>() where T : new()
+        {
+            var type = typeof(T);
+            var tableName = type.Name;
+            var query = $"SELECT * FROM {tableName}";
+            return ExecuteQuery<T>(query);
+        }
+        #endregion
+
     }
 }
