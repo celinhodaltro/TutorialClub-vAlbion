d261505dfaa9740bac3d78d88a9808562dc48851
João Marcelo Daltro Marques <joao.marques@produsti.com.br>
2024-06-03 11:37:47 -0300

    Preparação de authSystem (Steam)

--- TesteBlazorApp/Program.cs
+++ TesteBlazorApp/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddAuthorizationCore();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +22,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
